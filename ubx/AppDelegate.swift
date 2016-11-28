390e6095426477ec525d19ebcbe92a190643eaa7
Zeuxis Lo <seekstudio@gmail.com>
2016-11-28 18:11:58 +0800

    add base project files

---
+++ ubx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ubx
+//
+//  Created by Zeuxis on 28/11/2016.
+//  Copyright © 2016 Zeuxis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ubx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ubx
+//
+//  Created by Zeuxis on 28/11/2016.
+//  Copyright © 2016 Zeuxis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
